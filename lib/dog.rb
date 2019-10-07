12fe1a24ce3679e473f43c9e7665ca7565a4eabd
Will Hodgson <will.hodgson@current-rms.com>
2019-10-07 12:49:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,7 @@ class Dog
   end
 
   def self.find_or_create_by(dog)
-    DB[:conn].execute("SELECT * FROM dogs WHERE name = ? and breed = ?", dog["name"], dog["breed"])
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ? and breed = ?", dog["name"], dog["breed"])[0]
   end
 
 end
